da4b1b4f77a42c155b22afb77e2ffb5490eefd97
Nathan Labourse <pomilumi33@gmail.com>
2021-02-10 17:05:59 +0100

    add database call

--- app/src/main/java/com/infotech/infotech/MainActivity.java
+++ app/src/main/java/com/infotech/infotech/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,8 +21,14 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.tech_recycler);
 
+        DatabaseAccess databaseAccess = DatabaseAccess.getInstance(this);
+        databaseAccess.open();
+        List<String> quotes = databaseAccess.getContent();
+        databaseAccess.close();
+
         TechsAdapter techsAdapter = new TechsAdapter(this, s1, s2);
         recyclerView.setAdapter(techsAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
 }
