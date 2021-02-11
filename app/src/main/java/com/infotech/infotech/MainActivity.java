17f81b5aadc1527265a9fe1f15ee5f5d2c4bbbe5
Valentin Doche <contact@valentindoche.com>
2021-02-11 19:29:45 +0100

    Remove Import Unused

--- app/src/main/java/com/infotech/infotech/MainActivity.java
+++ app/src/main/java/com/infotech/infotech/MainActivity.java
@@ -1,6 +1,5 @@
 package com.infotech.infotech;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,18 +7,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
