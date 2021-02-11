6156d21d8c8686124aa9e912c8b814d63aedc149
arnaudbassey <arnaud.bassey@next-u.fr>
2021-02-11 23:53:22 +0100

    Update images static_content_courses

--- app/src/main/java/com/infotech/infotech/TechsAdapter.java
+++ app/src/main/java/com/infotech/infotech/TechsAdapter.java
@@ -2,9 +2,12 @@ package com.infotech.infotech;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -21,11 +24,11 @@ public class TechsAdapter extends RecyclerView.Adapter<TechsAdapter.ViewHolder> 
     final List<String> techValueName;
     final List<String> techValueType;
     final List<String> techValueDescription;
-    final List<String> techValueImg;
+    final List<byte[]> techValueImg;
     final List<String> techValueJobs;
     final Context context;
 
-    public TechsAdapter(Context ct, List<String> name,  List<String> type,  List<String> description,  List<String> img, List<String> jobs){
+    public TechsAdapter(Context ct, List<String> name,  List<String> type,  List<String> description,  List<byte[]> img, List<String> jobs){
         context = ct;
         techValueName = name;
         techValueType = type;
@@ -47,6 +50,9 @@ public class TechsAdapter extends RecyclerView.Adapter<TechsAdapter.ViewHolder> 
         holder.techName.setText(techValueName.get(position));
         holder.techDescription.setText(techValueDescription.get(position));
         holder.techJobs.setText(techValueJobs.get(position));
+
+        Bitmap bmp = BitmapFactory.decodeByteArray(techValueImg.get(position),0,techValueImg.get(position).length);
+        holder.techImg.setImageBitmap(Bitmap.createScaledBitmap(bmp, 128, 128,false));
 
         holder.techCard.setOnClickListener(view -> {
             Intent intent = new Intent(context, DetailsActivity.class);
@@ -71,6 +77,7 @@ public class TechsAdapter extends RecyclerView.Adapter<TechsAdapter.ViewHolder> 
         final Chip techJobs;
         final LinearLayout techItemLayout;
         final MaterialCardView techCard;
+        final ImageView techImg;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -79,6 +86,7 @@ public class TechsAdapter extends RecyclerView.Adapter<TechsAdapter.ViewHolder> 
             techJobs = itemView.findViewById(R.id.tech_offers);
             techItemLayout = itemView.findViewById(R.id.tech_item_layout);
             techCard = itemView.findViewById(R.id.tech_card);
+            techImg = itemView.findViewById(R.id.tech_image);
 
         }
     }
