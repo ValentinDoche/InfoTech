65faa16fc634dfbb2935f1a98b6a5969ebf8d9d8
Valentin Doche <contact@valentindoche.com>
2021-02-11 10:33:17 +0100

    Layout Details

---
+++ app/src/main/java/com/infotech/infotech/DetailsActivity.java
@@ -0,0 +1,14 @@
+package com.infotech.infotech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
---
+++ app/src/main/java/com/infotech/infotech/OpportunitiesAdapter.java
@@ -0,0 +1,4 @@
+package com.infotech.infotech;
+
+public class OpportunitiesAdapter {
+}
