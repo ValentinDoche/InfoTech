6e370ace41fc3e8d2521089a49fe3bc79aaa12fb
Valentin Doche <contact@valentindoche.com>
2021-02-11 18:11:15 +0100

    Link API Main Layout (#8)
    * Link API Main Layout
    
    * Delete JobsAPI.java
    
    # Unused
    
    Co-authored-by: Joriiss <78536901+Joriiss@users.noreply.github.com>

--- app/src/main/java/com/infotech/infotech/TechsAdapter.java
+++ app/src/main/java/com/infotech/infotech/TechsAdapter.java
@@ -9,19 +9,22 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.chip.Chip;
+
 import java.util.List;
 
 public class TechsAdapter extends RecyclerView.Adapter<TechsAdapter.ViewHolder> {
 
-    List<String> techValueName, techValueType, techValueDescription, techValueImg;
+    List<String> techValueName, techValueType, techValueDescription, techValueImg, techValueJobs;
     Context context;
 
-    public TechsAdapter(Context ct, List<String> name,  List<String> type,  List<String> description,  List<String> img){
+    public TechsAdapter(Context ct, List<String> name,  List<String> type,  List<String> description,  List<String> img, List<String> jobs){
         context = ct;
         techValueName = name;
         techValueType = type;
         techValueDescription = description;
         techValueImg = img;
+        techValueJobs = jobs;
     }
 
     @NonNull
@@ -36,6 +39,7 @@ public class TechsAdapter extends RecyclerView.Adapter<TechsAdapter.ViewHolder> 
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.techName.setText(techValueName.get(position));
         holder.techDescription.setText(techValueDescription.get(position));
+        holder.techJobs.setText(techValueJobs.get(position));
     }
 
     @Override
@@ -47,12 +51,13 @@ public class TechsAdapter extends RecyclerView.Adapter<TechsAdapter.ViewHolder> 
 
         TextView techName;
         TextView techDescription;
+        Chip techJobs;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             techName = itemView.findViewById(R.id.tech_name);
             techDescription = itemView.findViewById(R.id.tech_description);
-
+            techJobs = itemView.findViewById(R.id.tech_offers);
 
         }
     }
